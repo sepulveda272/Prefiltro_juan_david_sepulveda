2ed347213eab30a98f0f28e2987b98ebd09b2547
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2023-10-03 12:40:50 -0500

    feat: :tada: Se inicia el backend del proyecto

---
+++ Backend/database/config.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('DB ONLINE');
+    } catch (error) {
+        console.log(error);
+        throw new Error('DB no funcoin');
+    }
+}
+
+module.exports = {
+    dbConnection
+}
---
+++ Backend/models/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const {dbConnection} = require('../database/config.js');
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.pacientesPath = "/api/pacientes";
+        this.bacteriologosPath = "/api/bacteriologos";
+        this.cuadroHematicoPath = "/api/cuadroHematico";
+        this.glisemiaPath = "/api/glisemia";
+        this.perfilLipidicoPath = "/api/perfilLipidico";
+        this.searchPath = "/api/search";
+        this.connectDB();
+        this.middleware();
+        this.routes();
+    }
+
+    async connectDB(){
+        await dbConnection();
+    }
+
+    middleware(){
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes(){
+        this.app.use(this.pacientesPath, require('../routes/pacientes.routes.js'));
+        /* this.app.use(this.bacteriologosPath, require('../routes/bacteriologo.routes.js'));
+        this.app.use(this.cuadroHematicoPath, require('../routes/cuadroHematico.routes.js'));
+        this.app.use(this.glisemiaPath, require('../routes/glisemia.routes.js'));
+        this.app.use(this.perfilLipidicoPath, require('../routes/perfilLipidico.routes.js'));
+        this.app.use(this.searchPath, require('../routes/search.routes.js')); */
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log(`estamos en el puerto ${this.port}`);
+        })
+    }
+}
+
+module.exports = Server;
